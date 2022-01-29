5fabfc6813506cc444b7be1a81b474fb499075af
Danil <dannillav@mail.ru>
2022-01-29 12:49:33 +0500

    add object films in .js file and make up in pajes in .html file

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-const numberOfFilms = prompt('Сколько фильмов вы смотрели','');
+const numberOfFilms = prompt('Сколько фильмов вы смотрели', '');
 
+const personalMOvieDB = {
+    count: numberOfFilms,
+    movies: {
+        
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false
+}
+for (i = 0; i < 2; i++) {
+    const filmsName = prompt('Один из последних просмотренных фильмов', '');
+    const filmsRating = prompt('На сколько вы оцените фильм?', '');
+    personalMOvieDB.movies[filmsName] = `${filmsRating}`;
+}
