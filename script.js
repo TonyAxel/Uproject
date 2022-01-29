3befe287fb752803ea9f81deb7f50003158fe2bb
Danil <dannillav@mail.ru>
2022-01-29 18:10:21 +0500

    add new code and modification code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const numberOfFilms = prompt('Сколько фильмов вы смотрели', '');
+"use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы смотрели', '');
 const personalMOvieDB = {
     count: numberOfFilms,
     movies: {},
@@ -7,9 +8,24 @@ const personalMOvieDB = {
     genres: [],
     privat: false
 };
+
 for (let i = 0; i < 2; i++) {
     const filmsName = prompt('Один из последних просмотренных фильмов', '');
     const filmsRating = prompt('На сколько вы оцените фильм?', '');
-    personalMOvieDB.movies[filmsName] = `${filmsRating}`;
+
+    if (filmsName != "" && filmsName.length < 50 && filmsRating != "" && filmsName != null && filmsRating != null) {
+        personalMOvieDB.movies[filmsName] = `${filmsRating}`;
+    } else {
+        i--;
+    }
 }
+if (personalMOvieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMOvieDB.count >= 10 && personalMOvieDB.count < 30) {
+    console.log('Вы класический зритель');
+} else {
+    console.log("Вы киноман!");
+}
+
+
 console.log(personalMOvieDB);
